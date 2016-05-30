2b12caf67f8dc694ce663e9d58b4fc992d1c2ab4
amykangweb <selene6023@gmail.com>
2016-05-30 15:34:36 -0700

    Add database and meow model.

---
+++ Meowdit/DAL/MeowContext.cs
@@ -0,0 +1,13 @@
+﻿using Meowdit.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Meowdit.DAL
+{
+    public class MeowContext : DbContext
+    {
+        public MeowContext() : base("MeowContext") {}
+
+        public DbSet<Meow> Meows { get; set; }
+    }
+}
---
+++ Meowdit/Models/Meow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Meowdit.Models
+{
+    public class Meow
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string Word { get; set; }
+    }
+}
